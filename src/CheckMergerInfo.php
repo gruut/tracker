cbffcaeb51ab5cf1c5ac73b1e4d32b72401d10ed
blackiron <jjj@gruut.net>
2019-01-22 14:15:39 +0900

    # ADD - Merger에서 특정 Merger의 정보요청에 대한 응답
    - Merger에서 특정 Merger에 대한 정보를 제공하도록 함

---
+++ src/CheckMergerInfo.php
@@ -0,0 +1,31 @@
+<?php
+require_once('../utils/mysql-lib.php');
+require_once('../utils/MsgHandler.php');
+
+if(empty($_POST)){
+    return;
+}
+
+header("Content-type: application/json");
+$json_data = json_decode($_POST['message'],true);
+
+if(!checkMergerInfo($json_data)){
+    http_response_code(500);
+    $response_arr["flag"] = false;
+    echo json_encode($response_arr);
+    return;
+}
+
+$result_arr = mysql_open_($json_data);
+
+$merger_info = mysql_read_urecord_('merger', 'mID', $result_arr['mID']);
+if(!$merger_info){
+    http_response_code(500);
+    $response_arr["flag"] = false;
+    echo json_encode($response_arr);
+}
+else {
+    echo json_encode($merger_info);
+}
+mysql_close();
+
